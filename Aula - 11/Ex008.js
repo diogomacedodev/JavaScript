7afc4120811d38e2c656eab7b0991036af397ef4
Diogo Macedo <102424918+diogomacedodev@users.noreply.github.com>
2022-04-14 09:03:23 -0300

    aula 11

---
+++ Aula - 11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro e de ${vel}km/h`)
+if( vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida. Voce foi multado.')
+}
+console.log('Dirija sempre usando cinto de segurança')
