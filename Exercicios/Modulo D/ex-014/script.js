809565cc536c4241b3d14df6c59e7e508664ee51
Diogo Macedo <102424918+diogomacedodev@users.noreply.github.com>
2022-04-14 17:34:32 -0300

    atualizaçao

--- Exercicios/Modulo D/ex-014/script.js
+++ Exercicios/Modulo D/ex-014/script.js
@@ -4,24 +4,24 @@ var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
-}
-
 if ( hora>=0&&hora < 12){
     //Bom Dia
     img.src="img/manha.jpg"
     console.log +='Bom Dia'
-    document.body.style.background = '#1B998B'
+    document.body.style.background = '#BAFFED'
 
 }else if (hora >= 12 && hora < 18){
     //Boa Tarde
     
     img.src="img/tarde.jpg"
     console.log +='Boa Tarde'
-    document.body.style.backgroundColor = '#DECDF5'
+    document.body.style.backgroundColor = '#e4be82'
 
 }else if (hora < 24) {
     //Boa Noite
     
     img.src="img/noite.jpg"
     console.log +='Boa Noite'
+    document.body.style.backgroundColor = '#3029ad'
+}
 }
