a43daa63ccbc2875c8df3e4d5871e8b56e884950
Diogo Macedo <102424918+diogomacedodev@users.noreply.github.com>
2022-05-04 09:32:13 -0300

    ambiente js

---
+++ Aula - 13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Teste ${c}`)
+    c++
+}while (c <= 5)
+
+
+/*
+testa primeiro depois executa.
+
+var c = 1
+while (c <= 10){
+    console.log(`Teste ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Tudo Bem?');
+console.log('Tudo Bem?');
+console.log('Tudo Bem?');
+*/
